3162c4fa395c86851e2ec8079c56de49fd8df164
Jason McCallister <jason@craftcms.com>
2020-05-31 17:25:12 -0700

    tests and cleanup
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- hosts.go
+++ hosts.go
@@ -3,8 +3,15 @@ package hosts
 import (
 	"bufio"
 	"io"
+	"net"
 	"os"
+	"runtime"
 	"strings"
+)
+
+const (
+	WindowsHostsFileLocation = ""
+	NixHostsFilesLocation    = "/etc/hosts"
 )
 
 // Record represents a line in the hosts file. The record
@@ -19,7 +26,13 @@ type Record struct {
 
 func FindIP(ip string) ([]Record, error) {
 	var rcds []Record
-	f, err := open("/etc/hosts")
+
+	file := NixHostsFilesLocation
+	if runtime.GOOS == "windows" {
+		file = WindowsHostsFileLocation
+	}
+
+	f, err := open(file)
 	if err != nil {
 		return rcds, err
 	}
@@ -40,7 +53,13 @@ func FindIP(ip string) ([]Record, error) {
 
 func FindHost(host string) ([]Record, error) {
 	var rcds []Record
-	f, err := open("/etc/hosts")
+
+	file := NixHostsFilesLocation
+	if runtime.GOOS == "windows" {
+		file = WindowsHostsFileLocation
+	}
+
+	f, err := open(file)
 	if err != nil {
 		return rcds, err
 	}
@@ -65,7 +84,7 @@ func Read(rdr io.Reader) ([]Record, error) {
 	var records []Record
 
 	sc := bufio.NewScanner(rdr)
-	line := 1
+	var line int
 	for sc.Scan() {
 		txt := sc.Text()
 		line = line + 1
@@ -80,10 +99,22 @@ func Read(rdr io.Reader) ([]Record, error) {
 		if row := strings.Fields(txt); len(row) == 0 {
 			continue
 		} else {
-			// set the IP address
-			record.IP = row[0]
-			// append all hosts assigned to the IP
-			record.Hosts = row[1:]
+			r := row[0]
+			h := 1
+
+			if record.IsCommented {
+				r = row[1]
+				h = 2
+			}
+
+			// set the IP address only if a valid ip
+			ip := net.ParseIP(r)
+			if ip != nil {
+				record.IP = r
+
+				// append all hosts assigned to the IP only if the IP is valid
+				record.Hosts = row[h:]
+			}
 
 			records = append(records, record)
 		}
---
+++ hosts_test.go
@@ -0,0 +1,95 @@
+package hosts
+
+import (
+	"io"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestRead(t *testing.T) {
+	type args struct {
+		rdr io.Reader
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []Record
+		wantErr bool
+	}{
+		{
+			name: "can find records that are valid records but also commented out",
+			args: args{rdr: strings.NewReader(`# this is a comment
+127.0.0.1 domain.com example.com
+# 192.168.5.5 domain.com example.com`)},
+			want: []Record{
+				{
+					Line:        1,
+					IsCommented: true,
+					IP:          "",
+					Hosts:       nil,
+				},
+				{
+					Line:        2,
+					IsCommented: false,
+					IP:          "127.0.0.1",
+					Hosts:       []string{"domain.com", "example.com"},
+				},
+				{
+					Line:        3,
+					IsCommented: true,
+					IP:          "192.168.5.5",
+					Hosts:       []string{"domain.com", "example.com"},
+				},
+			},
+		},
+		{
+			name: "can find records that are valid records",
+			args: args{rdr: strings.NewReader(`# this is a comment
+127.0.0.1 domain.com example.com`)},
+			want: []Record{
+				{
+					Line:        1,
+					IsCommented: true,
+					IP:          "",
+					Hosts:       nil,
+				},
+				{
+					Line:        2,
+					IsCommented: false,
+					IP:          "127.0.0.1",
+					Hosts:       []string{"domain.com", "example.com"},
+				},
+			},
+		},
+		{
+			name: "can find records that have comments",
+			args: args{rdr: strings.NewReader(`# this is a comment`)},
+			want: []Record{
+				{
+					Line:        1,
+					IsCommented: true,
+					IP:          "",
+					Hosts:       nil,
+				},
+			},
+		},
+		{
+			name: "empty lines do not return a record",
+			args: args{rdr: strings.NewReader(``)},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Read(tt.args.rdr)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Read() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Read() got = \n%v, \nwant \n%v", got, tt.want)
+			}
+		})
+	}
+}
